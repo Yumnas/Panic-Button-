ba952f4fc4a53c842f338cd7d3b1a94b5bbfd334
yumna <yumnashabbir2@gmail.com>
2025-09-04 12:50:34 +0500

    pom.xml

---
+++ src/main/java/com/example/panicbuttonproject/PanicButtonProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.panicbuttonproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PanicButtonProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PanicButtonProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/panicbuttonproject/PanicButtonProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.panicbuttonproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PanicButtonProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
