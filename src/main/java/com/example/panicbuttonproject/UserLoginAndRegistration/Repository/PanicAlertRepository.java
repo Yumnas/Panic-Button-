4186014f1f81498cf295df1ca8ced23108dd9ee2
yumna <yumnashabbir2@gmail.com>
2025-09-05 15:09:03 +0500

    Added SQL query to handle foreign key in Panic Alert table

--- src/main/java/com/example/panicbuttonproject/UserLoginAndRegistration/Repository/PanicAlertRepository.java
+++ src/main/java/com/example/panicbuttonproject/UserLoginAndRegistration/Repository/PanicAlertRepository.java
@@ -2,11 +2,19 @@ package com.example.panicbuttonproject.UserLoginAndRegistration.Repository;
 
 import com.example.panicbuttonproject.UserLoginAndRegistration.Entity.PanicAlert;
 import com.example.panicbuttonproject.UserLoginAndRegistration.Entity.UserEntity;
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface PanicAlertRepository extends JpaRepository<PanicAlert, Long> {
     List<PanicAlert> findByUser(UserEntity user);
+    @Modifying
+    @Transactional
+    @Query("DELETE FROM PanicAlert p WHERE p.user.id = :userId")
+    void deleteByUserId(@Param("userId") long userId);
 
 }
