d8312a46b61b439b239a98a05761739bb3e86c72
yumna <yumnashabbir2@gmail.com>
2025-09-05 15:02:35 +0500

    Returning in BaseResponse

--- src/main/java/com/example/panicbuttonproject/UserLoginAndRegistration/Controller/PanicAlertController.java
+++ src/main/java/com/example/panicbuttonproject/UserLoginAndRegistration/Controller/PanicAlertController.java
@@ -1,13 +1,17 @@
 package com.example.panicbuttonproject.UserLoginAndRegistration.Controller;
 
-import com.example.panicbuttonproject.UserLoginAndRegistration.DTOclass.PanicAlertRequest;
+import com.example.panicbuttonproject.UserLoginAndRegistration.Entity.PanicAlert;
+import com.example.panicbuttonproject.UserLoginAndRegistration.Request.PanicAlertRequest;
+import com.example.panicbuttonproject.UserLoginAndRegistration.Response.BaseResponse;
+import com.example.panicbuttonproject.UserLoginAndRegistration.Response.PanicAlertResponse;
 import com.example.panicbuttonproject.UserLoginAndRegistration.Service.PanicAlertService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.security.core.Authentication;
+
+import java.util.Map;
 
 @RestController
 public class PanicAlertController {
@@ -16,10 +20,20 @@ public class PanicAlertController {
     private PanicAlertService panicAlertService;
 
     @PostMapping("/panic")
-    public ResponseEntity<String> sendPanicAlert(@RequestBody PanicAlertRequest request,
-                                                 Authentication authentication) {
-        String username = authentication.getName();
-        panicAlertService.sendPanicAlert(request, username);
-        return ResponseEntity.ok("Panic alert sent successfully!");
+    public BaseResponse<?> sendPanicAlert(@RequestBody PanicAlertRequest request,
+                                                   Authentication authentication) {
+        try {
+            if (authentication == null) {
+                return new PanicAlertResponse("401", "User not authenticated", null);
+            }
+
+            String username = authentication.getName();
+            PanicAlert alert = panicAlertService.sendPanicAlert(request, username);
+
+            return new BaseResponse<>("200", "Panic Alert Sent Successfully", Map.of("id", alert.getId()));
+
+        } catch (RuntimeException ex) {
+            return new PanicAlertResponse("400", ex.getMessage(), null);
+        }
     }
 }
