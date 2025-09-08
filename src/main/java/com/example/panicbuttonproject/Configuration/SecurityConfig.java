ddfc878abcaa9169b4e4f5854d3cc9a9c3dad06c
yumna <yumnashabbir2@gmail.com>
2025-09-08 15:21:29 +0500

    Made configuration package global

--- src/main/java/com/example/panicbuttonproject/Configuration/SecurityConfig.java
+++ src/main/java/com/example/panicbuttonproject/Configuration/SecurityConfig.java
@@ -1,4 +1,4 @@
-package com.example.panicbuttonproject.UserLoginAndRegistration.Configuration;
+package com.example.panicbuttonproject.Configuration;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
