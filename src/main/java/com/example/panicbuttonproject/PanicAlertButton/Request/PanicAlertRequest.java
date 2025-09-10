229f74757b4728a0fd69b984253526d234f49f22
yumna <yumnashabbir2@gmail.com>
2025-09-10 11:37:14 +0500

    Added longitude and latitude for location

--- src/main/java/com/example/panicbuttonproject/PanicAlertButton/Request/PanicAlertRequest.java
+++ src/main/java/com/example/panicbuttonproject/PanicAlertButton/Request/PanicAlertRequest.java
@@ -3,6 +3,8 @@ package com.example.panicbuttonproject.PanicAlertButton.Request;
 public class PanicAlertRequest {
     private String message;
     private String location;
+    private Double latitude;
+    private Double longitude;
 
     public String getMessage() {
         return message;
@@ -18,5 +20,21 @@ public class PanicAlertRequest {
 
     public void setLocation(String location) {
         this.location = location;
+    }
+
+    public Double getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(Double latitude) {
+        this.latitude = latitude;
+    }
+
+    public Double getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(Double longitude) {
+        this.longitude = longitude;
     }
 }
